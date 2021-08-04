e04c9a12cba3e6a513ca746b9ceebb52d03efd82
lee-sooyong <88360271+lee-sooyong@users.noreply.github.com>
2021-08-04 11:35:05 +0900

    Update class1.py

--- class1.py
+++ class1.py
@@ -2,7 +2,7 @@
 #클래스를 정의
 class Person():
     def __init__(self):
-        self.name="dafault name"
+        self.name="default name"
     def print(self):
         print("My name is {0}".format(self.name))
 
