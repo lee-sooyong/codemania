cc12ee0b7050f234510a1315687593b1ac3956dc
leesooyong <2sy96@naver.com>
2021-08-05 12:44:58 +0900

    파일처리

---
+++ try1.py
@@ -0,0 +1,22 @@
+# try1.py
+
+
+#함수 정의
+
+def divede(a,b):
+    return a/b
+
+#예외처리(에러처리)
+try:
+    #함수 호출
+    result = divede(5,0) 
+except ZeroDivisionError:
+    print("0으로 나누면 안됩니다.")
+except TypeError:
+    print("숫자여야 합니다.")
+else:
+    print("결과:{0}".format(result))
+finally:
+    print("무조건 실행")
+    
+print("전체 코드 실행 종료")
--- 다중상속01.py
+++ 다중상속01.py
@@ -1,20 +1,23 @@
 
 class Tiger:
     def jump(self):
-        print("호랑이 점프")
+        print("호랑이 점프")    
+    def cry(self):
+        print("호랑이 어흥")
 
 class Lion:
     def bite(self):
         print("사자 물어뜯기")
+    def cry(self):
+        print("사자 응애")
 
-class Liger(Tiger, Lion):
+class Liger(Lion,Tiger):
     def play(self):
         print("라이거와 놀기")
 
 
 l = Liger()
-l.play()
-l.jump()
-l.bite()
+l.cry()
+print("메서드 해석 순서:",Liger.__mro__)
 
         
